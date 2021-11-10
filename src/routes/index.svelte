36b11eb46a1c62bb7c5f3312662d0081cab29be0
Michael Ashton <mashton@ufl.edu>
2021-11-10 18:00:37 +0100

    first push

---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<script context="module">
+  import { GraphQLClient } from 'graphql-request';
+  export async function load() {
+    const graphcms = new GraphQLClient(
+      'https://api-eu-central-1.graphcms.com/v2/ckvtmnbn9001p01xsgk0j7j8h/master',
+      {
+        headers: {},
+      }
+    )
+    const { posts } = await graphcms.request(
+      `query Posts { 
+        posts {
+          title
+          content
+        }
+      }`
+    )
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let posts
+</script>
+
+<svelte:head>
+  <title>SvelteKit project template</title>
+</svelte:head>
+
+<h1>GraphCMS with SvelteKit and GraphQL Request</h1>
+
+{#each posts as post}
+  <p>{post.title}</p>
+  <p>{post.content}</p>
+{/each}
